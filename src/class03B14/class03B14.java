df1baa0f35d452950d540f0b1379b8c42d2f0c0e
CassieLopez98 <Cassandra.e.lopez3@gmail.com>
2022-09-27 19:50:23 -0400

    second commit

--- src/class03B14/class03B14.java
+++ src/class03B14/class03B14.java
@@ -4,5 +4,6 @@ public class class03B14 {
 	public static void main(String[] args) {
 	
 		System.out.println("hey this is a git class");
+	System.out.println("this is a change in my project");
 }
 }
